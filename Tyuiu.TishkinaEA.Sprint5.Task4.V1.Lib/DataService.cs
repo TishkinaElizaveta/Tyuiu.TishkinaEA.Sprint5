e514010d7bb103dfec3e2309ab928700ec954dcb
admin <admin@DESKTOP-LHFSSMF>
2024-12-13 22:58:54 +0500

    1

--- Tyuiu.TishkinaEA.Sprint5.Task4.V1.Lib/DataService.cs
+++ Tyuiu.TishkinaEA.Sprint5.Task4.V1.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.TishkinaEA.Sprint5.Task4.V1.Lib
             //double result = Convert.ToDouble((Math.Pow(x, -2) + 2) * Math.Sin(x));
             //return Math.Round(result, 2);            //string tempFilePath = Path.GetTempFileName();
             string tempFilePath = Path.GetTempFileName();
-            double x = Convert.ToDouble(File.ReadAllText(tempFilePath).Replace(",", "."));          
+            double x = 1;       
             double result = Convert.ToDouble(1 / ((Math.Cos(x) + x) - 4.12*x ));
             return Math.Round(result, 2);
         }
